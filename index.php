9f66a4e0867735b9f2e4d148a0a0c775d5c1ea98
Renato Alves Felix <renatofelix.redhat@gmail.com>
2019-03-27 11:44:52 -0300

    html ajust

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
 echo "Welcome to OpenShift Online Developer Preview";
-echo "<br>To test the database, hit the href='/dbtest.php' URL";
-echo "<br>To get php info, hit the href='/phpinfo.php' URL";
-
+echo "<br>To test the database, hit the <a href='/dbtest.php'>dbtest.php</a>URL";
+echo "<br>To get php info, hit the <a href='/phpinfo.php'>phpinfo.php</a>URL";
 
 echo "<br><br>This assumes that you have the correct env variables set";
 echo "<BR><BR>The environment variables required are databaseuser, databasepassword, and databasenmae.";
