0e72ad0585146ff5506beaf7b63809dc34011c1a
Renato Alves Felix <renatofelix.redhat@gmail.com>
2019-03-27 11:47:40 -0300

    wip php test db

--- dbtest.php
+++ dbtest.php
@@ -11,11 +11,28 @@ if ($connection->connect_errno) {
     exit();
 } else {
     printf("Connected to the database");
+    printf("SHOW TABLES:");
 
-    $result = mysql_query("show tables"); // run the query and assign the result to $result
-    while($table = mysql_fetch_array($result)) { // go through each row that was returned in $result
-        echo($table[0] . "<BR>");    // print the table that was returned on that row.
-    }    
+    //Connect to MySQL using the PDO object.
+    $pdo = new PDO('mysql:host=$dbhost;dbname=$dbname', '$dbuser', '$dbpwd');
+    
+    //Our SQL statement, which will select a list of tables from the current MySQL database.
+    $sql = "SHOW TABLES";
+    
+    //Prepare our SQL statement,
+    $statement = $pdo->prepare($sql);
+    
+    //Execute the statement.
+    $statement->execute();
+    
+    //Fetch the rows from our statement.
+    $tables = $statement->fetchAll(PDO::FETCH_NUM);
+    
+    //Loop through our table names.
+    foreach($tables as $table){
+        //Print the table name out onto the page.
+        echo $table[0], '<br>';
+    } 
 }
 $connection->close();
 ?>
